74dc644d5f163e28f9c584de260b23e72ff129e9
John Alawode <110995795+ostendoboss@users.noreply.github.com>
2023-08-09 18:52:43 +0100

    MySQL Advanced

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- SQL script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+
+CREATE TRIGGER decrement_trigger
+AFTER INSERT ON orders
+FOR EACH ROW
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser 
+-- hat computes and store the average score for a student.
+-- Note: An average score can be a decimal
+
+drop procedure IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INTEGER
+)
+BEGIN
+	UPDATE users
+	SET average_score=(SELECT AVG(score) FROM corrections
+		WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+END;$$
+DELIMITER ;
